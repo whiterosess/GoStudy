b6d5c0af5ca2a3301972574be848caa997eed0f4
whiterosess <69551907+whiterosess@users.noreply.github.com>
2020-09-22 11:42:18 +0700

    Create main.go
    1

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    name1 := "Ken the Ninja"
+    name2 := "Master White"
+    name3 := "Ben the Baby"
+    
+    // Print each string with line-break using \n
+    fmt.Printf("Welcome, %s\n", name1)
+    fmt.Printf("Welcome, %s\n", name2)
+    fmt.Printf("Welcome, %s\n", name3)
+}
+
